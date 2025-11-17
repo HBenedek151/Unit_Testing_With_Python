de68e9a0d09f0a707dc0473535fc346b2b32d835
HBenedek <horvath.benedek@diak.szbi-pg.hu>
2025-11-17 08:16:54 +0100

    calculator

---
+++ calculator.py
@@ -0,0 +1,15 @@
+def sum(x, y):
+    return x+y
+
+def subtract(x, y):
+    return x-y
+
+def multiply(x, y):
+    return x*y
+
+def divide(x, y):
+    try:
+        return x / y
+    except ZeroDivisionError:
+        return "Errror: Division by zero"
+    
---
+++ test_calculator.py
@@ -0,0 +1,15 @@
+import pytest
+from calculator import sum, subtract, multiply, divide
+
+def test_sum():
+    assert sum(2, 3) == 5
+
+#python -m pytest test_calculator.py -v
+
+def test_sb():
+    assert subtract(3, 2) == 1
+    assert subtract(-3, 2) == -5
+
+def test_mtp():
+    assert multiply(2, 3) == 6
+    assert multiply(-2 ,3) == -6
